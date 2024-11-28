18d58089f097b5353e8d917882e8b1d644b2326f
UdayKiran589 <i24ai003@aid.svnit.ac.in>
2024-11-28 14:34:45 +0530

    Create main76.c

---
+++ lab7/main76.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("enter a number \n");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("it is a even number");
+}
+else
+{
+printf("it is a odd number");
+}
+}
